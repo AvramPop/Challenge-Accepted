86d0a316d0fcfe8eebcebff33fdc7d072a29894b
Daniel Pop <Dany4god@gmail.com>
2017-02-04 12:04:58 +0200

    added project

---
+++ src/main/java/net/greetings/Sample.java
@@ -0,0 +1,12 @@
+package net.greetings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sample {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sample.class, args);
+    }
+}
