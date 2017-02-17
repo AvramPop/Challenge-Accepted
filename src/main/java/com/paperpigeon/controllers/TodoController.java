a1fef2718fa13d76c9dec259f0b1867d1254a758
Daniel Pop <Dany4god@gmail.com>
2017-02-17 16:28:46 +0200

    added basic security

--- src/main/java/com/paperpigeon/controllers/TodoController.java
+++ src/main/java/com/paperpigeon/controllers/TodoController.java
@@ -1,14 +1,26 @@
 package com.paperpigeon.controllers;
 
 import com.paperpigeon.dto.TodoDTO;
+import com.paperpigeon.dto.ValidationErrorDTO;
 import com.paperpigeon.exceptions.TodoNotFoundException;
 import com.paperpigeon.services.TodoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpStatus;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
+import java.util.Map;
 
 /**
  * This is the place where the whole magic happens. Here we create new REST calls
@@ -16,10 +28,11 @@ import java.util.List;
  */
 
 @RestController
-@RequestMapping("/api/todo")
+@RequestMapping("/todo")
 public final class TodoController {
 
     private final TodoService service;
+    private MessageSource messageSource;
 
     @Autowired
     TodoController(TodoService service) {
@@ -28,7 +41,7 @@ public final class TodoController {
 
     @RequestMapping(method = RequestMethod.POST)
     @ResponseStatus(HttpStatus.CREATED)
-    TodoDTO create(@RequestBody @Valid TodoDTO todoEntry) {
+    TodoDTO create(@RequestBody @Valid TodoDTO todoEntry){
         return service.create(todoEntry);
     }
 
---
+++ src/main/java/com/paperpigeon/error_handler/RestErrorHandler.java
@@ -0,0 +1,72 @@
+package com.paperpigeon.error_handler;
+
+import com.paperpigeon.dto.ValidationErrorDTO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.List;
+import java.util.Locale;
+
+@ControllerAdvice
+public class RestErrorHandler {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(RestErrorHandler.class);
+
+    private MessageSource messageSource;
+
+    @Autowired
+    public RestErrorHandler(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseBody
+    public ResponseEntity processValidationError(MethodArgumentNotValidException ex, HttpStatus status) {
+        LOGGER.debug("Handling form validation error");
+
+        BindingResult result = ex.getBindingResult();
+        List<FieldError> fieldErrors = result.getFieldErrors();
+
+        return ResponseEntity.status(status)
+                .body(fieldErrors);
+    }
+
+    private ValidationErrorDTO processFieldErrors(List<FieldError> fieldErrors) {
+        ValidationErrorDTO dto = new ValidationErrorDTO();
+
+        for (FieldError fieldError: fieldErrors) {
+            String localizedErrorMessage = resolveLocalizedErrorMessage(fieldError);
+            LOGGER.debug("Adding error message: {} to field: {}", localizedErrorMessage, fieldError.getField());
+            dto.addFieldError(fieldError.getField(), localizedErrorMessage);
+        }
+
+        return dto;
+    }
+
+    private String resolveLocalizedErrorMessage(FieldError fieldError) {
+        Locale currentLocale = LocaleContextHolder.getLocale();
+        String localizedErrorMessage = messageSource.getMessage(fieldError, currentLocale);
+
+        //If a message was not found, return the most accurate field error code instead.
+        //You can remove this check if you prefer to get the default error message.
+        if (localizedErrorMessage.equals(fieldError.getDefaultMessage())) {
+            String[] fieldErrorCodes = fieldError.getCodes();
+            localizedErrorMessage = fieldErrorCodes[0];
+        }
+
+        return localizedErrorMessage;
+    }
+}
